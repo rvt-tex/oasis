08df3c66a6ec8ee0c6c32f9f395f4bdef737979c
github username <github email address>
2020-04-06 18:01:31 -0400

    updated route.rb with controller action and uri

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'sessions#home'
+
+  get '/register' => 'clients#new'
+  post 'register' => 'clients#create'
+
+
+
+
+
   resources :clients
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
