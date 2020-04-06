50147a41c9f71011bf4921e214648e5bb3f79795
github username <github email address>
2020-04-06 18:20:13 -0400

    updated client and sessions controller with create method

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,5 +4,19 @@ class ClientsController < ApplicationController
     end 
 
     def create 
+        @client = Client.new(client_params)
+        if @client.save 
+            session[:client_id] = @client.id
+            redirect_to @client
+        else 
+            render :new 
+        end 
     end 
+
+    def show 
+    end 
+
+    def client_params
+        params.require('client').permit(:first_name, :last_name, :password,:phone,:email)
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+
+    def home
+    end 
+
+    def new 
+    end 
+    
+    def create 
+        @client = Client.find_by(email:params[:client][:email])
+        if @client && @client.authenticate(params[:client][:password])
+            session[:client_id] = @client.id
+            redirect_to  @client
+        else 
+            flash[:message] = "incorrect login info, please try again"
+            redirect_to "/login"
+        end 
+    end 
 end
