0344f3a980cdf1027f83bee9c111db377745fdc8
github username <github email address>
2020-04-06 18:27:04 -0400

    added helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :is_logged_in?
+    helper_method :logged_in?
+    helper_method :current_client 
+ 
+   # def current_client
+   #     @current_client ||= Client.find_by_id(session[:client_id]) if session[:client_id]
+   # end
+
+   def current_client
+       Client.find_by(id: session[:client_id])
+   end
+
+   def is_logged_in?
+       !!session[:client_id]
+   end
+
+   def require_login
+       unless is_logged_in?
+         flash[:message] = "Login Required."
+         redirect_to '/' 
+       end
+   end
+   
+     def redirect_if_not_logged_in
+         redirect_to '/' if !logged_in? 
+     end 
+
+   def logged_in?
+       !current_client.nil?  
+   end
 end
