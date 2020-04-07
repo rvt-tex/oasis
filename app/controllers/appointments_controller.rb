0c52687a6db761290413998ff65d980665e4242d
github username <github email address>
2020-04-06 21:17:11 -0400

    updated method in new & create method and created appointment_params

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,8 +1,23 @@
 class AppointmentsController < ApplicationController
 
     def new 
+        @appointment = Appointment.new  
     end 
 
     def create 
+        #binding.pry
+        @appointment = current_client.appointments.build(appointment_params)
+        binding.pry
+        if @appointment.save 
+            redirect_to @appointment
+        else 
+            render :new 
+        end 
     end 
+
+    private 
+
+    def appointment_params
+        params.require('appointment').permit(:specail_request, :desired_time, :desired_date, :client, treatment_ids:[])
+    end
 end
