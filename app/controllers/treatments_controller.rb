0747a8cc17226ec0dd65dc9c29a82978ac29cdf7
github username <github email address>
2020-04-06 20:15:29 -0400

    add new, create, show actions in contoller

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -1,2 +1,29 @@
 class TreatmentsController < ApplicationController
+
+    def index 
+    end 
+
+    def new 
+        @treatment = Treatment.new  
+    end 
+
+    def create
+        #binding.pry
+        @treatment = Treatment.new(treatment_params)
+        if @treatment.save 
+            redirect_to @treatment
+        else 
+            render :new 
+        end 
+    end 
+
+    def show
+        @treatment = Treatment.all 
+    end 
+
+    private 
+
+    def treatment_params
+        params.require('treatment').permit(:title, :price, :duration, :description)
+    end
 end
