57100915e4b5e767f24e9e85cdf14ecad81348e3
github username <github email address>
2020-04-07 01:59:52 -0400

    updated appointment controller, views

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,14 @@
 class AppointmentsController < ApplicationController
 
     def new 
-        @appointment = Appointment.new  
+        @appointment = Appointment.new 
+       
     end 
 
     def create 
-        
+        binding.pry 
         @appointment = current_client.appointments.build(appointment_params)
-        binding.pry
+       
         if @appointment.save 
             redirect_to @appointment
         else 
@@ -15,12 +16,13 @@ class AppointmentsController < ApplicationController
         end 
     end 
 
+
     def show 
     end 
 
     private 
 
     def appointment_params
-        params.require('appointment').permit(:specail_request, :desired_time, :desired_date, :client, treatment_id:[])
+        params.require('appointment').permit(:special_request, :desired_time, :desired_date, :client, :treatment_id)
     end
 end
