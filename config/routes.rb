78c936aa9708e1d43ce056a72a8693a2b2732e21
github username <github email address>
2020-04-07 21:11:08 -0400

    updated routes and add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,23 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  resources :clients
   resources :treatments
+
+
+  resources :clients do 
+    resources :appointments, only: [:show, :index]
+  end 
+
   resources :appointments
+  
+  # resources :clients, only: [:show] do 
+  #   resources :appointments, only: [:show, :index]
+  # end 
+  
+  # resources :appointments, only: [:index, :show, :new, :create, :edit, :update]
+  
+  # root 'appointments#index'
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
