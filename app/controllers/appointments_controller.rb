ac04a09337e1375944ffad4deff62fd13edc3768
github username <github email address>
2020-04-07 21:12:41 -0400

    updated index and show controller action

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,12 +1,53 @@
 class AppointmentsController < ApplicationController
 
+    def index
+        if params[:client_id]
+          client = Client.find_by(id:params[:client_id])
+          if client
+            @appointments = client.appointments
+          else
+            flash[:alert] = "Client not found."
+            redirect_to clients_path
+          end
+        else
+          @appointments = Appointment.all
+        end
+    end
+
+    def show
+        if params[:client_id]
+          client = Client.find_by(id:params[:client_id])
+          @appointment = client.appointments.find_by(id:params[:id])
+          if !@appointment
+            flash[:alert] = "Appointment not found."
+            redirect_to client_appointments_path(client)
+          end
+        else
+          @appointment = Appointment.find(params[:id])
+        end
+    end
+    # def index 
+    #     # if params[:client_id]
+    #     #     @appointments = Client.find(params[:client_id]).appointments
+    #     # else 
+    #     #     @appointments = Appointment.all 
+    #     # end 
+
+
+    #     if params[:client_id] && @client = Client.find_by_id(params[:client_id])
+    #         @appointments = @client.appointments
+    #     else
+    #         @error = "That client doesn't exist" if params[:client_id]
+    #         @appointments = Appointment.all
+    #     end
+    # end 
+
     def new 
         @appointment = Appointment.new 
-       
     end 
 
     def create 
-        binding.pry 
+        #binding.pry 
         @appointment = current_client.appointments.build(appointment_params)
        
         if @appointment.save 
@@ -16,13 +57,15 @@ class AppointmentsController < ApplicationController
         end 
     end 
 
+   
 
-    def show 
-    end 
+    # def show 
+    #     @appointment = Appointment.find(params[:id])
+    # end 
 
     private 
 
     def appointment_params
-        params.require('appointment').permit(:special_request, :desired_time, :desired_date, :client, :treatment_id)
+        params.require('appointment').permit(:special_request, :desired_time, :desired_date, :client_id, :treatment_id)
     end
 end
