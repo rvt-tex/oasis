33a22cff1638fc942e5d78a05c02433628edb62f
github username <github email address>
2020-04-10 05:32:38 -0400

    added application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def display_date(d)
+        d.strftime('%A, %B %d, %Y')
+    end
+    
+    def display_time(d)
+        d.strftime('%I:%M %p')
+    end
 end
--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,19 +1,11 @@
 module AppointmentsHelper
-
-    def index_display_header
-        if @client
-            content_tag(:h2, "#{@client.first_name}'s Appointments:")
+    
+    def display_appointments 
+        if @client.appointments.empty?
+            content_tag(:h5, link_to('No appointments yet - Book an Appointment here', new_appointment_path))
         else
-            content_tag(:h3, "All Appointments")
+            client = @client == current_client ? 'You have' : "#{@client.first_name}'s"
+            content_tag(:h4, "#{client} #{pluralize(@client.appointments.count, 'booked Appointments')}:")
         end
     end
-    
-    # def display_appointments 
-    #     if @client.appointments.empty?
-    #         tag.h2(link_to('No appointments yet - Book an Appointment here', new_appointment_path))
-    #     else
-    #         client = @client == current_user ? 'Your' : "#{@client.first_name}'s"
-    #         content_tag(:h2, "#{client} #{pluralize(@client.appointments.count, 'Appointment')}:")
-    #     end
-    # end
 end
