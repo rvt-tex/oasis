f4c4eecd1d1e408dcf1e8d9219aac6977ea929f7
github username <github email address>
2020-04-11 01:53:56 -0400

    updated review controller and views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,30 @@
 class ReviewsController < ApplicationController
 
     def index
+        @review = Review.all
     end 
 
-    def new 
+    def new
+        @review = Review.new  
     end 
 
     def create 
+        @review = Review.new(review_params)
+        if @review.save 
+            redirect_to @review
+        else 
+            binding.pry
+            render :new 
+        end 
     end 
 
     def show 
+        @review = Review.find_by(params[:id])
+    end 
+
+    private 
+
+    def review_params
+        params.require('review').permit(:content, :client_id)
     end 
 end
