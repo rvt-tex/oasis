78a2a8dd21611977203f85d8ff22d5a7013f5f93
github username <github email address>
2020-04-11 00:04:08 -0400

    updated clients model showing association between review and client

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,7 @@ class Client < ApplicationRecord
 
     has_secure_password
 
+    has_many :reviews
     has_many :appointments
     has_many :treatments, through: :appointments 
 
@@ -12,15 +13,4 @@ class Client < ApplicationRecord
 
     
    
-
-
-    # def self.google(auth)
-    # #find_or_create a client using the attributes auth
-    #     where(email: auth.info.email).first_or_initialize do |client|
-    # #     @client = Client.find_or_create_by(email: auth["info"]["email"]) do |client|
-    # #     client.first_name = auth["info"]["first_name"]
-    # #     client.last_name = auth["info"]["last_name"]
-    # #     client.password = SecureRandom.hex(10)
-    #     end 
-    # end 
 end
