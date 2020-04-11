d5cdcffe3b859fd97e1f6b08be11d839e91c7d55
github username <github email address>
2020-04-10 23:11:19 -0400

    added scope method in model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,7 +5,7 @@ class Appointment < ApplicationRecord
 
     validates :treatment, :desired_time, :desired_date, presence: true
 
-    # scope :alpha, -> { order(:appointments) }
+    scope :alpha, -> { order(:desired_date) }
     # scope :most_booked_treatments, -> {left_joins(:treatments).group('appointments.id').order('count(treatments.appointment_id) desc')}
     
     #accepts_nested_attributes_for 
