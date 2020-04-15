2e32f722a034a63cc361c6e4fd6dc187fb5824be
github username <github email address>
2020-04-14 23:09:14 -0400

    updated routes with nested resources of appointments & reviews

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,12 @@ Rails.application.routes.draw do
   end 
 
   resources :appointments
-
+  
+  resources :appointments do
+    resources :reviews
+  end
+  
+  resources :reviews
   
    
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
