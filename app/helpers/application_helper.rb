9a72c7eb0f653b3e857dbefd7370fa1f58b59315
github username <github email address>
2020-04-16 04:29:50 -0400

    refactoring and validations as i update code base

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,34 @@ module ApplicationHelper
     def title(text)
         content_for :title, text
     end
+
+    # def body_container_class
+    #     "container mt-5"
+    # end 
+
+    # def main_class
+    #     'row align-items-center justify-content-center bg-transparent py-4'
+    # end
+
+    # def card
+    #     content_tag :div, class: card_class do
+    #       yield
+    #     end
+    # end
+
+    # def card_header
+    #     content_tag :div, class: card_header_class do
+    #       yield
+    #     end
+    # end
+
+    # def card_title_class
+    #     'card-title display-4 text-light font-title'
+    # end
+
+    # def err
+    #     content_for :err do
+    #       yield
+    #     end
+    # end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,8 +5,19 @@ class Appointment < ApplicationRecord
     has_many :reviews
     has_many :clients, through: :reviews
     
-
     validates :treatment, :desired_time, :desired_date, presence: true
+
+
+    validate :desired_date_cannot_be_in_the_past
+ 
+    def desired_date_cannot_be_in_the_past
+        if desired_date.present? && desired_date < Date.today
+            errors.add(:desired_date, "can't be in the past")
+        end
+    end 
+
+
+
 
     scope :alpha, -> { order(:desired_date) }
     # scope :most_booked_treatments, -> {left_joins(:treatments).group('appointments.id').order('count(treatments.appointment_id) desc')}
